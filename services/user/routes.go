645887297940056097af4c27930827e1460593d8
Junior <eecfredes@gmail.com>
2024-05-22 22:26:58 -0300

    :rocket: Types|User register types

--- services/user/routes.go
+++ services/user/routes.go
@@ -1,8 +1,10 @@
 package user
 
 import (
+	"encoding/json"
 	"net/http"
 
+	"github.com/JMustang/warehouse/types"
 	"github.com/gorilla/mux"
 )
 
@@ -21,5 +23,13 @@ func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
 
 }
 func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
+	// get JSON payload
+	if r.Body == nil {
 
+	}
+	var payload types.RegisterUserPayload
+	err := json.NewDecoder(r.Body).Decode(payload)
+	// check if the user already exists
+	// create a new user
+	// return JWT
 }
